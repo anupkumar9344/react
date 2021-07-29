bbfe2ed6d1efaee264f3ace456ff714e20e70595
anupkumar9344 <anupkumar9344@gmail.com>
2021-07-29 21:33:19 +0530

    Creating component

---
+++ src/Heading.js
@@ -0,0 +1,12 @@
+import './index.css';
+import React from 'react'
+
+
+function Heading() {
+    return (
+        <div>
+            <h1>Hello, Welcome to Website World</h1>
+        </div>
+    )
+}
+export default Heading
---
+++ src/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+function List() {
+    return (
+        <div>
+            <ol>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ol>
+        </div>
+    )
+}
+export default List
